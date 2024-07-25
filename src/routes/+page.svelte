be0cb973c90fba90474563e1d3cef7515d315fbf
kanzoonrekza <kanzoonrkz@gmail.com>
2024-07-25 10:14:51 +0700

    install tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
+<p>
+	Visit <a class="text-blue-700 hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to
+	read the documentation
+</p>
