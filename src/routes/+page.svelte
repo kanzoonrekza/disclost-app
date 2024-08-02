dc1cdceccb8e23715fb16341f75313c693ac2125
kanzoonrekza <kanzoonrkz@gmail.com>
2024-08-02 15:58:49 +0700

    slicing main layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
 <p>
-	Visit <a class="text-blue-700 hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to
-	read the documentation
+	Visit
+	<a class="text-blue-700 hover:underline" href="https://kit.svelte.dev">kit.svelte.dev</a>
+	to read the documentation
 </p>
