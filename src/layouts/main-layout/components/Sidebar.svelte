b9c16c96ae6f9b40a883c0bbf403bbdd7801ee3b
kanzoonrekza <kanzoonrkz@gmail.com>
2024-08-10 19:18:19 +0700

    config ui for first minimal integration

--- src/layouts/main-layout/components/Sidebar.svelte
+++ src/layouts/main-layout/components/Sidebar.svelte
@@ -1,8 +1,8 @@
 <aside class="overflow-auto">
 	<ul>
 		<li><a href="/dashboard">Home</a></li>
-		<li><a href="/categories">Categories</a></li>
+		<!-- <li><a href="/categories">Categories</a></li> -->
 		<li><a href="/recycle-bin">Recycle Bin</a></li>
-		<li>Folder Tree</li>
+		<!-- <li>Folder Tree</li> -->
 	</ul>
 </aside>
